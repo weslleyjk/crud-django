14780f8ff3c78e39139e99033c3560389fcfc747
Weslley Félix <wesleystark@gmail.com>
2021-11-26 23:16:07 -0300

    criando o app e o models do site

---
+++ appblog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model): 
+    title = models.CharField(max_length=255) # 255 caracteres max
+    slug = models.SlugField(max_length=255, unique = True) # url abaixo do titulo
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
+    body = models.TextField() # texto do post
+    created = models.DateTimeField(auto_now_add=True) # adiciona a data e a hr no post criado
+    updated = models.DateTimeField(auto_now=True) # atualiza a data de uma modificacao
+
+    
+    
+
