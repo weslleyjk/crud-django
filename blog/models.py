5d5c0c9e1703dd9c7be6673c286dc9c4d008c7e1
Weslley <weslleystark@gmail.com>
2021-12-07 12:18:20 -0300

    editando templates e adicionando css

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model): 
     title = models.CharField(max_length=255) # 255 caracteres max
@@ -9,7 +10,14 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True) # adiciona a data e a hr no post criado
     updated = models.DateTimeField(auto_now=True) # atualiza a data de uma modificacao
 
+
+    class meta:
+        ordering = ("-created",) # ordenar de acordo com o horario da criacao do post
+
     def __str__(self):
         return self.title
      
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={"slug": self.slug}) # criar o link de direcionamento.
+    
     
